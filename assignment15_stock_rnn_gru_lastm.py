5ef3a9c7dcb424b5c3ec42ad453ec048c42983a1
Bishesh-Bhandari <bisheshbhandari14@gmail.com>
2026-05-06 13:47:32 +0545

    sucessfully dowloaded data using yfinance

--- assignment15_stock_rnn_gru_lastm.py
+++ assignment15_stock_rnn_gru_lastm.py
@@ -7,6 +7,9 @@ import urllib.request        # Download dataset without extra dependencies
 import json                  
 from collections import Counter  # Count word frequencies for vocabulary
 
+import yfinance as yf
+
+
 import numpy as np           
 import torch                
 import torch.nn as nn       
@@ -51,3 +54,61 @@ print(f"   Device object: {DEVICE}")
 
 # we need to set the hyperparameters which are shared across rnn/lstm/gru
 
+STOCKS = ["AAPL", "TSLA"]          # two stocks
+START_DATE = "2018-01-01"
+END_DATE   = "2023-12-31"
+FEATURE_COL = "Close"              #   we took closing price only as mention in assignment 
+
+
+SEQ_LEN = 60                       # Use past 60 trading days to predict next day
+BATCH_SIZE = 64
+TRAIN_SPLIT = 0.8
+
+
+INPUT_SIZE = 1                  # only close price
+HIDDEN_SIZE = 64
+NUM_LAYERS = 2
+DROPOUT = 0.2
+
+# Training
+NUM_EPOCHS = 20
+LEARNING_RATE = 0.001
+CLIP_GRAD_NORM = 5.0
+
+print("Hyperparameters loaded")
+
+# now we need to dowload the data n which we need to work on
+def download_data(stocks,start_date,end_date):
+    stock_data = {}
+
+    for stock in stocks:
+        print(f"downloading {stock} data....")
+        df = yf.download(stock,start =start_date ,end =end_date,progress =False)
+
+        df = df[[FEATURE_COL]].dropna()
+        
+        df.columns = ["Close"]
+
+        stock_data[stock] = df
+        print(f"{stock}: {len(df)} rows")
+
+    return stock_data
+
+stock_data = download_data(STOCKS, START_DATE, END_DATE)
+
+total_steps = sum(len(df) for df in stock_data.values())
+print(f"\nTotal time steps across stocks: {total_steps}")
+
+#visualization of the raw data using matplotlib
+
+plt.figure(figsize=(12, 5))
+
+for ticker, df in stock_data.items():
+    plt.plot(df.index, df["Close"], label=ticker)
+
+plt.title("Historical Closing Prices")
+plt.xlabel("Date")
+plt.ylabel("Close Price")
+plt.legend()
+plt.grid(True)
+plt.show()
